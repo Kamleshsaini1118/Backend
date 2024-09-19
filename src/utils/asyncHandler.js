8966afb80d608c1db3257972f89779c66e6a9eeb
Kamlesh Saini <kamleshsaini1345@gmail.com>
2024-09-19 12:50:58 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export { asyncHandler }
+
+// // try catch code for raper
+// // function mein function pass kr diya
+
+/* const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+         res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+         })
+    }
+} */
+

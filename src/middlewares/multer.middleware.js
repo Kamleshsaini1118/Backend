abeb0e9c17ad9e8b55d077f6d3263cf31315fedf
Kamlesh Saini <kamleshsaini1345@gmail.com>
2024-09-20 22:36:04 +0530

    file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname) // bad approce because same name ki file bhut sare aa shekte h to usse file duplicate ho jayge
+    }
+  })
+  
+export  const upload = multer({ storage, })
---
+++ src/models/user.model.js
@@ -0,0 +1,101 @@
+import mongoose, {Schema} from "mongoose";  
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true // index use for searching purpose.
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+
+        avatar: {
+            type: String, // use the cloudinary URL 
+            required: true,
+        },
+
+        coverImage: {
+            type: String, // use the cloudinary URL
+        },
+
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+// for password encryption before save the password
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// custom hook/methods for ki password sahi hain ya nhi 
+userSchema.methods.isPassowrdCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+} 
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
